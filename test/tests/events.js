cd613c253d99c381827407d87cb9273c8b1e4924
Michael D. Norman <michael.norman@denormans.com>
2016-05-28 15:51:24 -0500

    Added event commands

---
+++ test/tests/events.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const mcpi = require('../../lib/minecraft.js');
+const helpers = require('../utils/helpers');
+
+describe('Events', () => {
+  it('eventsBlocksHits', (done) => {
+    const mc = helpers.mc();
+
+    mc.eventsBlockHits()
+      .then(data => {
+        console.log('eventsBlockHits data:', data);
+      })
+      .then(() => mc.close())
+      .then(done)
+      .catch(err => {
+        mc.close();
+        done.fail(err);
+      });
+  });
+
+  it('eventsClear', (done) => {
+    const mc = helpers.mc();
+
+    mc.eventsClear()
+      .then(() => mc.close())
+      .then(done)
+      .catch(err => {
+        mc.close();
+        done.fail(err);
+      });
+  });
+});
