7da41138b02608fd90f1e6fd7de1558ca3dfe49d
Michael D. Norman <michael.norman@denormans.com>
2016-05-28 09:42:40 -0500

    Added getBlock

--- test/tests/getBlocks.js
+++ test/tests/getBlocks.js
@@ -3,6 +3,22 @@
 const helpers = require('../utils/helpers');
 
 describe('Get blocks', () => {
+  it('getBlock', (done) => {
+    const mc = helpers.mc();
+
+    mc.getBlock(99999, 99999, 99999)
+      .then(receivedData => {
+        expect(receivedData).not.toBeNull();
+        expect(receivedData).toEqual(0);
+      })
+      .then(() => mc.close())
+      .then(done)
+      .catch(err => {
+        mc.close();
+        done.fail(err);
+      });
+  });
+
   it('getBlockWithData', (done) => {
     const mc = helpers.mc();
 
